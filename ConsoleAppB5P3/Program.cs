e66389b207eede65f946fc32f596a7a1ec934c4d
Wanderiel <dpavel82@yandex.ru>
2023-03-08 15:43:24 +0300

    Добавлена треться задача

---
+++ ConsoleAppB5P3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleAppB3P4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ExitCommand = "exit";
+            const string CodeWordCommand = "sum";
+
+            string userInput;
+
+            List<int> numbers = new List<int>();
+
+            bool isWorking = true;
+
+            Console.WriteLine($"Вводите любые числа." +
+                $"\nДля подсчёта суммы введите - {CodeWordCommand}, для выхода - {ExitCommand}.");
+
+            while (isWorking)
+            {
+                Console.Write("Введите число: ");
+                userInput = Console.ReadLine().ToLower();
+
+                switch (userInput)
+                {
+                    case ExitCommand:
+                        isWorking = false;
+                        break;
+
+                    case CodeWordCommand:
+                        int sum = 0;
+
+                        foreach (int value in numbers)
+                            sum += value;
+
+                        Console.WriteLine($"\nСумма всех введённых чисел: {sum}");
+                        isWorking = false;
+                        break;
+
+                    default:
+                        if (int.TryParse(userInput, out int number))
+                            numbers.Add(number);
+                        break;
+                }
+            }
+
+            Console.WriteLine("\nВсего доброго!");
+            Console.ReadKey();
+        }
+    }
+}
