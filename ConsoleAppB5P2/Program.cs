48dba3aa22f209a159abfab137961d749dc867ee
Wanderiel <dpavel82@yandex.ru>
2023-03-08 15:05:21 +0300

    Добавлена вторая задача

---
+++ ConsoleAppB5P2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace ConsoleAppB5P2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 9;
+
+            Queue<int> purchases = new Queue<int>();
+
+            GetFillQueue(purchases, size);
+
+            int revenue = CalculationRevenue(purchases);
+
+            Console.WriteLine($"Все клиенты обслужены, выручка составила: {revenue}");
+            Console.ReadKey();
+        }
+
+        private static void GetFillQueue(Queue<int> purchases, int size)
+        {
+            Random random = new Random();
+
+            int minRandom = 1;
+            int maxRandom = 1001;
+
+            for (int i = 0; i < size; i++)
+                purchases.Enqueue(random.Next(minRandom, maxRandom));
+        }
+
+        private static int CalculationRevenue(Queue<int> purchases)
+        {
+            int sum = 0;
+
+            foreach (int purchase in purchases)
+            {
+                sum += purchase;
+
+                Console.WriteLine($"Сумма по чеку: {purchase}\nТекущая выручка: {sum}");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            return sum;
+        }
+    }
+}
